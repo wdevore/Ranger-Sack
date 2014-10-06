fe08f984ff8135f78bb368607ef3a91574163510
William DeVore <william.quartz@gmail.com>
2014-10-06 16:18:42 -0400

    migrated from Ranger-Dart
    All unit tests, app, nodes and templates are located in Ranger-Dart

---
+++ web/applications/rocket/game/game_manager.dart
@@ -0,0 +1,31 @@
+part of ranger_rocket;
+
+//import 'dart:async';
+//
+//import '../resources/resources.dart';
+
+class GameManager {
+  static final GameManager _instance = new GameManager._internal();
+
+  GameManager._internal();
+  
+  Resources _resources = new Resources();
+
+  // ----------------------------------------------------------
+  // Instance
+  // ----------------------------------------------------------
+  static GameManager get instance => _instance;
+
+  Resources get resources => _resources;
+  bool get isBootResourcesReady => resources.isBootResourcesReady;
+  bool get isBaseResourcesReady => resources.isBaseResourcesReady;
+
+  Future bootInit() {
+    return _resources.loadBootResources();
+  }
+  
+  Future baseInit() {
+    return _resources.loadBaseResources();
+  }
+
+}
